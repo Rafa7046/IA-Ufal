1d9a2735da47bc7666918c51b42711b5051d0dae
Rafael <rafael-vilella@hotmail.com>
2022-05-01 14:48:31 -0300

    Final fixes

--- Metro_de_Paris/A_star_search.py
+++ Metro_de_Paris/A_star_search.py
@@ -1,15 +1,18 @@
 from numpy import empty
 from Metro import Metro
 
+def find_min(border):
+    min = border[0].get_current_time()
+    index = 0
+    for i in range(len(border)):
+        if border[i].get_current_time() < min:
+            min = border[i].get_current_time()
+            index = i
+    return border[index]
+
 def A_star_search(border, final_station):
     while border is not empty:
-        min = border[0].get_current_time()
-        index = 0
-        for i in range(len(border)):
-            if border[i].get_current_time() < min:
-                min = border[i].get_current_time()
-                index = i
-        metro = border[index]
+        metro = find_min(border)
         if metro.operator[0] == final_station:
                 metro.trace.pop(0)
                 return (metro.trace, metro.time)
@@ -20,7 +23,5 @@ def A_star_search(border, final_station):
 
 def route(initial_station, line, final_station):
     initial_operator = (-1, initial_station, 0, line)
-    border = []
-    first_metro = Metro(-1, 0, [], initial_operator)
-    border.append(first_metro)
+    border = [Metro(-1, 0, [], initial_operator)]
     return A_star_search(border, final_station)
